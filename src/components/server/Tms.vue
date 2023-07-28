2e15a015d0cbfcaccdd22192f88cb32006e4d331
majun <2113725201@qq.com>
2023-07-28 17:45:26 +0800

    组件目录

---
+++ src/components/server/Tms.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-button type="primary" @click="flyToLocation">FlyTwo</el-button>
+</template>
+
+<script>
+
+export default {
+    name: "FlyToLocationButton",
+    data: () => ({}),
+    methods: {
+        flyToLocation() {
+            // 在这里添加跳转到地球指定位置的逻辑
+            const { Cartesian3 } = Cesium;
+            const { Rectangle } = Cesium;
+            const { HeadingPitchRange } = Cesium;
+            const center = Cartesian3.fromDegrees( // 指定要跳转的经纬度
+                91.97385008333333,
+                27.596977972222223,
+                5000
+            );
+            Rectangle.fromCartesianArray([center]);
+            window.cesiumViewer.camera.flyTo({
+                destination: center,
+                orientation: new HeadingPitchRange(0, -Math.PI / 2, 0),
+            });
+        },
+        toTms() {
+            // 添加akka发布的tms服务
+            viewer.imageryLayers.addImageryProvider(
+                new Cesium.UrlTemplateImageryProvider({
+                    url: "http://192.168.163.131:9090/map/{z}/{x}/{y}",
+                    format: "image/png",
+                    maximumLevel: 18,
+                })
+            );
+            layer.alpha = 1.0;
+            //两倍亮度
+            layer.brightness = 1.0;
+        }
+    },
+};
+</script>
