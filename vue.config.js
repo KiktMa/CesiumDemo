bc7ebc8ee748dffd3a29ed888b082917fafb42e5
majun <2113725201@qq.com>
2023-07-29 16:17:24 +0800

    解决连接nginx中资源跨域问题

--- vue.config.js
+++ vue.config.js
@@ -15,17 +15,39 @@ module.exports = {
 			toUrlUndefined: true
 		},
 		plugins: [
-			new CopyWebpackPlugin([
-				{ from: path.join(cesiumSource, cesiumjs), to: 'Cesium/Cesium.js' },
-				{ from: path.join(cesiumSource, cesiumThirdParty), to: 'Cesium/ThirdParty' },
-				{ from: path.join(cesiumSource, 'Assets'), to: 'Cesium/Assets' },
-				{ from: path.join(cesiumSource, 'Widgets'), to: 'Cesium/Widgets' },
-				{ from: path.join(cesiumSource, cesiumWorkers), to: 'Cesium/Workers' }
+			new CopyWebpackPlugin([{
+					from: path.join(cesiumSource, cesiumjs),
+					to: 'Cesium/Cesium.js'
+				},
+				{
+					from: path.join(cesiumSource, cesiumThirdParty),
+					to: 'Cesium/ThirdParty'
+				},
+				{
+					from: path.join(cesiumSource, 'Assets'),
+					to: 'Cesium/Assets'
+				},
+				{
+					from: path.join(cesiumSource, 'Widgets'),
+					to: 'Cesium/Widgets'
+				},
+				{
+					from: path.join(cesiumSource, cesiumWorkers),
+					to: 'Cesium/Workers'
+				}
 			]),
 			new webpack.DefinePlugin({
 				CESIUM_BASE_URL: JSON.stringify('Cesium')
 			})
 		]
-	}
+	},
+	devServer: {
+		proxy: {
+			'/3dtiles': {
+				target: 'http://localhost:8000', // Nginx服务器的地址和端口
+				changeOrigin: true,
+			},
+		},
+	},
 
 }
